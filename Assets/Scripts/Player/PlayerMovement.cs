f3cdb3e35a257e37e3da3066f3d73265cd890cad
Ssssoar <jano20@vtr.net>
2023-06-12 20:41:38 -0400

    Modify the Player movement
    Made it Transform based rather than RigidBody based.
    Should make things simpler to implement since it's basic RPG based movement.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,20 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour{
     public float speed = 5f;
 
-    private Rigidbody2D rb;
-
-    private void Start() {
-        rb = GetComponent<Rigidbody2D>();
+    private void Update(){
     }
 
-    public void Move(float horizontal, float vertical) {
+    public void Move(float horizontal, float vertical){
         Vector2 movement = new Vector2(horizontal, vertical);
         movement.Normalize();
-
-        rb.velocity = movement * speed;
+        transform.position += (Vector3)movement * speed * Time.deltaTime;
     }
 }
