2ab69d99582525c101327ecf6669b62297670ce4
Ssssoar <jano20@vtr.net>
2023-06-20 22:35:02 -0400

    Colisiones simples
    MUY simples, pero para el tipo de juego deberían ser mas que suficiente

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,6 +9,13 @@ public class PlayerMovement : MonoBehaviour{
     public void Move(float horizontal, float vertical){
         Vector2 movement = new Vector2(horizontal, vertical);
         movement.Normalize();
-        transform.position += (Vector3)movement * speed * Time.deltaTime;
+        //                                       Start position      Size        angle  direction              distance
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position , transform.localScale , 0 , movement , movement.magnitude * speed * Time.deltaTime);
+        Debug.Log(movement.magnitude * speed * Time.deltaTime);
+        if(hit.collider == null){
+            transform.position += (Vector3)movement * speed * Time.deltaTime;
+        }else{
+            //transform.position += (Vector3)movement * hit.distance * 0.1f;
+        }
     }
 }
