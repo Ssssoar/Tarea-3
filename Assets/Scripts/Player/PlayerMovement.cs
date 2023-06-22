9a2277f957239bcb986fd9f8a3e725f106efd4b5
Ssssoar <jano20@vtr.net>
2023-06-22 08:13:51 -0400

    glep
    gloop

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,11 +11,9 @@ public class PlayerMovement : MonoBehaviour{
         movement.Normalize();
         //                                       Start position      Size        angle  direction              distance
         RaycastHit2D hit = Physics2D.BoxCast(transform.position , transform.localScale , 0 , movement , movement.magnitude * speed * Time.deltaTime);
-        Debug.Log(movement.magnitude * speed * Time.deltaTime);
         if(hit.collider == null){
             transform.position += (Vector3)movement * speed * Time.deltaTime;
         }else{
-            //transform.position += (Vector3)movement * hit.distance * 0.1f;
         }
     }
 }
